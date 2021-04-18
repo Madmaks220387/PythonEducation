1c665043dd97b03df2bac40e1966acd02acf0ba0
Maxim <madmaks220387@gmail.com>
2021-04-18 12:59:00 +0300

    add lesson 1.1

---
+++ Lesson_1/Lesson_1.1.py
@@ -0,0 +1,13 @@
+# Задание 1
+
+x = 100
+y = 200
+
+print(f"Переменные х и y - {x} и {y}")
+
+str1 = input("Введите строку 1: ")
+str2 = input("Введите строку 2: ")
+numb1 = int(input("Введите число 1: "))
+numb2 = int(input("Введите число 2: "))
+
+print(f"Ваши данные: str1 = {str1}, str2 = {str2}, numb1 = {numb1}, numb2 = {numb2}")
